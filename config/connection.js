d9b8bd3369e463cce00bab8dd308973a654529e3
rosey-flo <emilyflo2012@yahoo.com>
2024-10-14 21:06:51 -0400

    changed pg db name

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,29 @@
+// const { Sequelize } = require('sequelize');
+
+// const client = process.env.DB_URL ? new Sequelize(process.env.DB_URL, {
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false
+//     }
+//   }
+// }) : new Sequelize(
+//   process.env.LOCAL_DB_NAME,
+//   process.env.LOCAL_DB_USERNAME,
+//   process.env.LOCAL_DB_PASSWORD, {
+//   host: 'localhost',
+//   dialect: 'postgres',
+//   logging: false
+// });
+
+
+
+// module.exports = client;
+
 const { Sequelize } = require('sequelize');
 
 const client = process.env.PG_URL ? new Sequelize(process.env.PG_URL, {
+  dialect: 'postgres', // Ensure dialect is specified
   dialectOptions: {
     ssl: {
       require: true,
@@ -11,11 +34,9 @@ const client = process.env.PG_URL ? new Sequelize(process.env.PG_URL, {
   process.env.LOCAL_DB_NAME,
   process.env.LOCAL_DB_USERNAME,
   process.env.LOCAL_DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'postgres',
-  logging: false
+    host: 'localhost',
+    dialect: 'postgres',
+    logging: false
 });
-
-
 
 module.exports = client;
