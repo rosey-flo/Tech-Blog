ea548520773cf11c935ea1ad7c8a728bd1c71af3
rosey-flo <emilyflo2012@yahoo.com>
2024-10-14 21:16:47 -0400

    changed pg db name

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //PACKAGES NEEDED IN YOUR SERVER JS - PACKAGE IMPORTS/DEPENDENCIES
 require('dotenv').config();
+console.log('Connecting to database at:', process.env.PG_URL);
+
 const express = require('express');
 const session = require('express-session');
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
